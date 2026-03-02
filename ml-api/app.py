d0d338fd32bf9977164e1a021bdeb19e14b011a0
Suvarna Bhinthaa K <suvarnabhinthaak.ug22.it@francisxavier.ac.in>
2026-03-02 16:35:20 +0530

    new one

--- ml-api/app.py
+++ ml-api/app.py
@@ -861,4 +861,5 @@ if __name__ == '__main__':
     print("=" * 50)
     print("Running on http://127.0.0.1:8000")
     print("=" * 50)
-    app.run(host='0.0.0.0', port=8000, debug
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
